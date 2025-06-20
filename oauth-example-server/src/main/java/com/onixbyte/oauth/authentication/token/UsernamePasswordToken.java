024fcc54a4fc2c926ec1f725928b3ad6124a6f1b
zihluwang <really@zihlu.wang>
2025-06-20 19:16:53 +0800

    feat: add user information

--- oauth-example-server/src/main/java/com/onixbyte/oauth/authentication/token/UsernamePasswordToken.java
+++ oauth-example-server/src/main/java/com/onixbyte/oauth/authentication/token/UsernamePasswordToken.java
@@ -1,50 +1,74 @@
 package com.onixbyte.oauth.authentication.token;
 
+import com.onixbyte.oauth.data.persistent.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class UsernamePasswordToken implements Authentication, CredentialsContainer {
+
+    private String username;
+
+    private String password;
+
+    private User user;
+
+    private boolean authenticated;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return Collections.emptyList();
+    }
+
+    public void setCredentials(String credentials) {
+        this.password = credentials;
     }
 
     @Override
-    public Object getCredentials() {
-        return null;
+    public String getCredentials() {
+        return password;
+    }
+
+    public void setDetails(User details) {
+        this.user = details;
     }
 
     @Override
-    public Object getDetails() {
-        return null;
+    public User getDetails() {
+        return user;
+    }
+
+    public void setPrinciple(String principle) {
+        this.username = principle;
     }
 
     @Override
-    public Object getPrincipal() {
-        return null;
+    public String getPrincipal() {
+        return username;
     }
 
     @Override
     public boolean isAuthenticated() {
-        return false;
+        return authenticated;
     }
 
     @Override
-    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
-
+    public void setAuthenticated(boolean authenticated) throws IllegalArgumentException {
+        this.authenticated = authenticated;
     }
 
     @Override
     public String getName() {
-        return "";
+        return username;
     }
 
     @Override
     public void eraseCredentials() {
-
+        this.password = "";
+        this.user.setPassword("");
     }
 }
