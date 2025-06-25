12397a98bb7951859feceb1789653652408bb43a
siujamo <jamo.siu@outlook.com>
2025-06-25 10:20:40 +0800

    fix: fix package name error

--- oauth-example-server/src/main/java/com/onixbyte/oauth/repository/UserRepository.java
+++ oauth-example-server/src/main/java/com/onixbyte/oauth/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.onixbyte.oauth.repository;
 import com.mybatisflex.core.BaseMapper;
 import com.onixbyte.oauth.model.User;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +13,10 @@ public interface UserRepository extends BaseMapper<User> {
 
     boolean canRegister(@Param("user") User user);
 
+    @Select("""
+            SELECT *
+              FROM users
+             WHERE msal_open_id = #{msalOpenId}
+            """)
     Optional<User> getUserByMsalOpenId(@Param("msalOpenId") String msalOpenId);
 }
